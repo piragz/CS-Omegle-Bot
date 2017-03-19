2422ab998872a85a872b4c60bb0fd8b3f0a52cd2
Aditya Joshi <adi.joshi194@gmail.com>
2017-03-19 11:17:05 +0530

    Add files via upload

--- CS.Omegle.Bot.py
+++ CS.Omegle.Bot.py
@@ -14,7 +14,7 @@ os.system('cls' if os.name == 'nt' else 'clear')
 space(8);print "---------------------------------------------"
 space(8);print "| CyberSucks Omegle Spam Bot                |"
 space(8);print "| Coded by D@rk TruTH                       |"
-space(8);print "| Version : 1                               |"
+space(8);print "| Version : 1.1                             |"
 space(8);print "| CyberSucks.TK                             |"
 space(8);print "| Twitter @darktruth190                     |"	
 space(8);print "| Facebook: Fb.me/CyberSec007               |"
@@ -26,14 +26,41 @@ msg1 = raw_input("Enter your first message (1/4) >> ")
 msg2 = raw_input("Enter your second message (2/4) >> ")
 msg3 = raw_input("Enter your third message (3/4) >> ")
 msg4 = raw_input("Enter your fourth message (4/4) >> ")
-display = Display(visible=1, size=(800, 600))
-display.start()
-driver = webdriver.Chrome()
-driver.get('http://www.omegle.com')
-interest1 = driver.find_element_by_xpath('//input[@class="newtopicinput"]')
-interest1.send_keys(interest)
-btn = driver.find_element_by_id("textbtn")
-btn.click()
+p = raw_input("Enter y to use proxy >> ")
+if p == "y":
+	prox = raw_input("Enter proxy in format of ip:port >> ")
+	display = Display(visible=1, size=(800, 600))
+	display.start()
+	PROXY = prox
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('--proxy-server=socks5://' + PROXY)
+	driver = webdriver.Chrome(chrome_options=chrome_options)
+	driver.get('http://www.omegle.com')
+	interest1 = driver.find_element_by_xpath('//input[@class="newtopicinput"]')
+	interest1.send_keys(interest)
+	btn = driver.find_element_by_id("textbtn")
+	btn.click()
+	while True:
+		time.sleep(8)
+		try:
+			main()
+		except InvalidElementStateException:
+			main2()
+elif p == "n":
+	display = Display(visible=1, size=(800, 600))
+	display.start()
+	driver = webdriver.Chrome()
+	driver.get('http://www.omegle.com')
+	interest1 = driver.find_element_by_xpath('//input[@class="newtopicinput"]')
+	interest1.send_keys(interest)
+	btn = driver.find_element_by_id("textbtn")
+	btn.click()
+	while True:
+		time.sleep(8)
+		try:
+			main()
+		except InvalidElementStateException:
+			main2()
 
 def main():
 	try:
@@ -94,9 +121,3 @@ def main2():
 	except InvalidElementStateException:
 		main2()
 
-while True:
-	time.sleep(8)
-	try:
-		main()
-	except InvalidElementStateException:
-		main2()
